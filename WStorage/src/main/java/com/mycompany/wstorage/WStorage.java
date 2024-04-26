67409d8e2ac267db0e0bee8d6f820b012af7bde3
Igor Stein <igorstein2001@gmail.com>
2024-04-26 14:45:28 -0300

    Criando o repositório

---
+++ WStorage/src/main/java/com/mycompany/wstorage/WStorage.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wstorage;
+
+/**
+ *
+ * @author Igor Stein
+ */
+public class WStorage {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
